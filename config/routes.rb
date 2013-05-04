62807a2037251c3b6958bfdabc62c69ed394a882
Mwatha Bwanali <mwathabwanali@gmail.com>
2013-05-04 14:52:50 +0200

    adding placeholder for IE

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 XInventory::Application.routes.draw do
-  get "home/index"
+  #get "user/login"
+
+  resources :user do                                                          
+    member do                                                                   
+      get :remote, :action => :login                                    
+    end                                                                         
+                                                                                
+    collection do                                                               
+      post :login
+      get :login
+    end                                                                         
+  end
 
   resources :home do                                                          
     member do                                                                   
@@ -70,5 +81,5 @@ XInventory::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-  root :to => 'home#index'
+  root :to => 'user#login'
 end
