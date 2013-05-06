584356a87852b409e80cf4bb6c28acf17939c7a4
Mwatha Bwanali <mwathabwanali@gmail.com>
2013-05-06 19:10:25 +0200

    creating assets

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -25,10 +25,43 @@ class AssetsController < ApplicationController
       [state.name , state.id]
     end
 
+    @suppliers = Supplier.order('name ASC').collect do |supplier|
+      [supplier.name , supplier.id]
+    end
+
+    @location = Site.order('name ASC').collect do |site|
+      [site.name , site.id]
+    end
 
   end
 
   def create
+    Item.transaction do
+      item = Item.new()
+      item.name = params[:asset]['name']
+      item.category_type = params[:asset]['category']
+      item.brand = params[:asset]['manufacturer']
+      item.version = params[:asset]['version']
+      item.serial_number = params[:asset]['serial_num']
+      item.vendor = params[:vendor]['supplier']
+      item.model = params[:asset]['model']
+      item.project_id = params[:work]['project']
+      item.donor_id = params[:work]['donor']
+      item.purchased_date = params[:vendor]['date_of_invoice'].to_date
+      item.order_number = params[:vendor]['invoice_num']
+      item.quantity = params[:vendor]['quantity']
+      item.cost = params[:vendor]['cost']
+      item.date_of_receipt = params[:organisation]['receipt_date'].to_date
+      item.delivered_by = params[:organisation]['delivered_by']
+      item.status_on_delivery = params[:organisation]['delivery_status']
+      item.location = params[:organisation]['location']
+      if item.save 
+        flash[:notice] = 'Successfully created.'                                  
+      else                                                                        
+        flash[:error] = 'Something went wrong - did not create.'                  
+      end
+    end
+    redirect_to '/create_new_asset'
   end
 
   def new_category
