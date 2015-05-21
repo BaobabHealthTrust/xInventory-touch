38d9112eb04433754dd3b93b48038ccb895b33a0
timmtonga <timmtonga@gmail.com>
2015-05-21 14:16:04 +0200

    Added version display on application using git tags and exempted some file from being tracked

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
+APP_VERSION = `git describe`.gsub("\n", "")
 # Initialize the rails application
 XInventory::Application.initialize!
